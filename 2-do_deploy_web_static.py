0041861d56539d6a33890860ebdbdef2e2c6e4c8
Bosirex <dennis.dancan@gmail.com>
2023-10-05 05:51:25 -0700

    AirBnB clone - MySQL

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
+Fabric scripts distributes an archive to the web servers
 """
 
+import os
 from datetime import datetime
 from fabric.api import *
-import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["54.157.191.113", "18.206.197.140"]
 env.user = "ubuntu"
 
 
 def do_pack():
     """
-        return the archive path if archive has generated correctly.
+        return archive path if archive was generated successful.
     """
 
     local("mkdir -p versions")
@@ -29,24 +29,23 @@ def do_pack():
 
 def do_deploy(archive_path):
     """
-        Distribute archive.
+        distributes an archive to the web servers.
     """
     if os.path.exists(archive_path):
         archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        new_version = "/data/web_static/releases/" + archived_file[:-4]
         archived_file = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo mkdir -p {}".format(new_version))
         run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
+                                             new_version))
         run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo mv {}/web_static/* {}".format(new_version,
+                                                new_version))
+        run("sudo rm -rf {}/web_static".format(new_version))
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+        run("sudo ln -s {} /data/web_static/current".format(new_version))
 
         print("New version deployed!")
         return True
-
     return False
