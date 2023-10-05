1da7583801c0e219f74c93a994494f32785e2873
Bosirex <dennis.dancan@gmail.com>
2023-10-05 13:02:48 -0700

    AirBnB clone - MySQL

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -33,18 +33,18 @@ def do_deploy(archive_path):
     """
     if os.path.exists(archive_path):
         archived_file = archive_path[9:]
-        new_version = "/data/web_static/releases/" + archived_file[:-4]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
         archived_file = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(new_version))
+        run("sudo mkdir -p {}".format(newest_version))
         run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             new_version))
+                                             newest_version))
         run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(new_version,
-                                                new_version))
-        run("sudo rm -rf {}/web_static".format(new_version))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(new_version))
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
         print("New version deployed!")
         return True
