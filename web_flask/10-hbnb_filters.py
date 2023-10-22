37d1387b50a3a1125c31c0384d8965092b99db6c
Bosirex <dennis.dancan@gmail.com>
2023-10-22 11:04:02 -0700

    AirBnB clone - Web framework

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 @app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
+def display_hbnb_filters():
     """Displays the main HBnB filters HTML page."""
     states = storage.all("State")
     amenities = storage.all("Amenity")
